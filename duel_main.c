d217e648a4fc81f3464e2879c84833ed19fac3d3
Tail <vitaliycof@outlook.com>
2025-08-20 14:33:42 +0300

    -Minor changes.

--- duel_main.c
+++ duel_main.c
@@ -2,18 +2,19 @@
 
 #include "duel_main.h"
 #include "duel_debug.h"
-//#include "duel_ssd1306.h"
 
-static int duel_major = 0;
-static int duel_minor = 0;
-static int duel_chrdev_count = 3;
+#define CHARDEV_COUNT 1
+
+static int char_major = 0;
+static int char_minor = 0;
+static int blk_major = 0;
 
 static void duel_exit(void) {
-    dev_t devno = MKDEV(duel_major, duel_minor);
+    dev_t devno = MKDEV(char_major, char_minor);
     PDEBUG("Duel: killing SPI...\n");
     //duel_ssd1306_exit();
     PDEBUG("Duel: cleaning module memory...\n");
-    unregister_chrdev_region(devno, duel_chrdev_count);
+    unregister_chrdev_region(devno, CHARDEV_COUNT);
     //kfree(devices);
 }
 
@@ -24,17 +25,12 @@ static int __init duel_init(void) {
     PDEBUG("Duel: initialization started...\n");
 
     //Динамическая инициализация области для символьных устройств.
-    result = alloc_chrdev_region(&dev, duel_minor, duel_chrdev_count, "duel");
+    result = alloc_chrdev_region(&dev, char_minor, CHARDEV_COUNT, "duel_char");
     if (result < 0) {
-        printk(KERN_WARNING "Duel: couldn't choose chrdev_region.\n");
+        printk(KERN_WARNING "Duel: couldn't allocate chrdev_region.\n");
         return result;
     }
     duel_major = MAJOR(dev);
-
-    /*result = duel_ssd1306_init();
-    if (result) {
-        goto fault;
-    }*/
 
     //devices = kmalloc(sizeof(struct duel_led_device) * duel_chrdev_count, GFP_KERNEL);
     //if (!devices) {
