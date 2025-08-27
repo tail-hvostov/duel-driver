95a97ca25976b0d34fb4b4c00524372b80a4861c
Tail <vitaliycof@outlook.com>
2025-08-27 14:06:21 +0300

    -Added a new unit test.

---
+++ tests/fast.c
@@ -0,0 +1,29 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+
+char buf[400];
+
+int main(int argc, const char* argv[]) {
+    int fast;
+
+    puts("1. Writing 360 bytes.");
+    fast = open("/dev/duel1", O_WRONLY);
+    if (fast < 0) {
+        puts("The file did not open.");
+        goto fault;
+    }
+    if (360 != write(fast, buf, 360)) {
+        puts("Couldn't write 360 bytes.");
+        close(fast);
+        goto fault;
+    }
+    close(fast);
+
+    puts("Success!");
+    return 0;
+fault:
+    puts("Failure!");
+    return 1;
+}
