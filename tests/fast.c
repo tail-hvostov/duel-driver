7acc6e03681079af5189e6eb3915bf7e841ab314
Tail <vitaliycof@outlook.com>
2025-08-27 14:17:13 +0300

    -Added new unit tests.

--- tests/fast.c
+++ tests/fast.c
@@ -21,6 +21,32 @@ int main(int argc, const char* argv[]) {
     }
     close(fast);
 
+    puts("2. Attempting to write 400 bytes.");
+    fast = open("/dev/duel1", O_WRONLY);
+    if (fast < 0) {
+        puts("The file did not open.");
+        goto fault;
+    }
+    if (400 == write(fast, buf, 400)) {
+        puts("400 bytes were written.");
+        close(fast);
+        goto fault;
+    }
+    close(fast);
+
+    puts("3. Writing 360 bytes with O_NONBLOCK.");
+    fast = open("/dev/duel1", O_WRONLY, O_NONBLOCK);
+    if (fast < 0) {
+        puts("The file did not open.");
+        goto fault;
+    }
+    if (360 != write(fast, buf, 360)) {
+        puts("Couldn't write 360 bytes.");
+        close(fast);
+        goto fault;
+    }
+    close(fast);
+
     puts("Success!");
     return 0;
 fault:
