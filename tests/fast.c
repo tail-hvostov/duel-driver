fdddf7783bcaae0ec9320fdda63074138f366662
Tail <vitaliycof@outlook.com>
2025-08-28 12:20:52 +0300

    -Updated unit tests.

--- tests/fast.c
+++ tests/fast.c
@@ -3,7 +3,30 @@
 #include <unistd.h>
 #include <errno.h>
 
-char buf[400];
+#define BUF_SIZE 400
+
+char buf[BUF_SIZE];
+
+void fill_buf(void) {
+    int i;
+    char val = 0;
+    for (i = 0; i < 360; i++) {
+        buf[i] = val;
+        val++;
+    }
+}
+
+int check_buf(void) {
+    int i;
+    char val = 0;
+    for (i = 0; i < 360; i++) {
+        if (val != buf[i]) {
+            return 0;
+        }
+        val++;
+    }
+    return 1;
+}
 
 int main(int argc, const char* argv[]) {
     int fast;
@@ -27,9 +50,35 @@ int main(int argc, const char* argv[]) {
         puts("The file did not open.");
         goto fault;
     }
-    if (400 == write(fast, buf, 400)) {
+    if (BUF_SIZE == write(fast, buf, BUF_SIZE)) {
         puts("400 bytes were written.");
         close(fast);
+        goto fault;
+    }
+    close(fast);
+
+    puts("3. Read & write test.");
+    fast = open("/dev/duel1", O_WRONLY);
+    if (fast < 0) {
+        puts("The file did not open.");
+        goto fault;
+    }
+    fill_buf();
+    if (360 != write(fast, buf, 360)) {
+        puts("Couldn't write 360 bytes.");
+        close(fast);
+        goto fault;
+    }
+    close(fast);
+    fast = open("/dev/duel1", O_RDONLY);
+    memset(buf, 0, 360);
+    if (360 != read(fast, buf, 360)) {
+        puts("Couldn't read 360 bytes.");
+        close(fast);
+        goto fault;
+    }
+    if (!check_buf()) {
+        puts("Buffer check failed.");
         goto fault;
     }
     close(fast);
