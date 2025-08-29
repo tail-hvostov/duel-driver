5138e373093c6684419303886cafcf80d93fe0f2
Tail <vitaliycof@outlook.com>
2025-08-29 23:49:10 +0300

    -Minor changes.

--- tests/simple.c
+++ tests/simple.c
@@ -8,34 +8,58 @@
 
 char buf[BUF_SIZE];
 
-int main(int argc, const char* argv[]) {
+int fill_ff(void) {
+    int i;
     int fast;
+    for (i = 0; i < 360; i++) {
+        buf[i] = 0xFF;
+    }
 
-    puts("1. Writing 360 bytes.");
-    fast = open("/dev/duel2", O_WRONLY);
+    fast = open("/dev/duel1", O_WRONLY);
     if (fast < 0) {
-        puts("The file did not open.");
-        goto fault;
+        return 1;
     }
     if (360 != write(fast, buf, 360)) {
-        puts("Couldn't write 360 bytes.");
         close(fast);
-        goto fault;
+        return 2;
     }
     close(fast);
+    return 0;
+} 
 
-    puts("2. Attempting to write 400 bytes.");
-    fast = open("/dev/duel2", O_WRONLY);
-    if (fast < 0) {
+int main(int argc, const char* argv[]) {
+    int simple;
+
+    puts("1. Writing 360 bytes.");
+    simple = open("/dev/duel2", O_WRONLY);
+    if (simple < 0) {
         puts("The file did not open.");
         goto fault;
     }
-    if (BUF_SIZE == write(fast, buf, BUF_SIZE)) {
-        puts("400 bytes were written.");
-        close(fast);
+    if (360 != write(simple, buf, 360)) {
+        puts("Couldn't write 360 bytes.");
+        close(simple);
         goto fault;
     }
-    close(fast);
+    close(simple);
+
+    puts("2. Attempting to write 400 bytes.");
+    simple = open("/dev/duel2", O_WRONLY);
+    if (simple < 0) {
+        puts("The file did not open.");
+        goto fault;
+    }
+    if (BUF_SIZE == write(simple, buf, BUF_SIZE)) {
+        puts("400 bytes were written.");
+        close(simple);
+        goto fault;
+    }
+    close(simple);
+
+    if (fill_ff()) {
+        puts("Fast device doesn't work.");
+        goto fault;
+    }
 
     puts("Success!");
     return 0;
