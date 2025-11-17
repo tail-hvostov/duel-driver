e5d1e7a6602dadc6b8a7b06131af51e009393da4
Tail <vitaliycof@outlook.com>
2025-11-17 15:21:56 +0300

    -Updated script for testing.

--- scripts/duel_test.sh
+++ scripts/duel_test.sh
@@ -1,8 +1,13 @@
 counter=1
+failed=0
 for test in tests/compiled/*.out
 do
     name=$(basename --suffix=".out" $test)
-    echo "Test $counter: $name"
+    echo "Test suite $counter: $name"
     counter=$((counter + 1))
     sudo $test
+    exit_code=$?
+    failed=$((failed+exit_code))
 done
+counter=$((counter - 1))
+echo "Failed $failed/$counter tests."
