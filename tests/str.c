e30228aa8129e5c20e99f16a4eefe34bcd24ca1c
Tail <vitaliycof@outlook.com>
2025-12-01 00:27:12 +0300

    -Added 2 basic tests.

--- tests/str.c
+++ tests/str.c
@@ -10,6 +10,8 @@
 int main() {
     int str;
     off_t buf_size;
+    off_t test_buf_size;
+    char* test_buf = nullptr;
 
     puts("1. Getting buffer size through llseek.");
     str = open(STR_FILE, O_RDONLY);
@@ -27,11 +29,40 @@ int main() {
     printf("Buffer size is %jd.\n", (intmax_t)buf_size);
     close(str);
 
+    test_buf_size = buf_size + 20;
+    test_buf = new char[test_buf_size];
+
+    printf("2. Writing %u bytes.\n", buf_size);
+    str = open(STR_FILE, O_WRONLY);
+    if (str < 0) {
+        puts("The file did not open.");
+        goto fault;
+    }
+    if (buf_size != write(str, buf, buf_size)) {
+        printf("Couldn't write %u bytes.\n", buf_size);
+        close(str);
+        goto fault;
+    }
+    close(str);
+
+    printf("3. Attempting to write %u bytes.\n", test_buf_size);
+    str = open(STR_FILE, O_WRONLY);
+    if (str < 0) {
+        puts("The file did not open.");
+        goto fault;
+    }
+    if (test_buf_size == write(str, buf, test_buf_size)) {
+        printf("%u bytes were written.\n", test_buf_size);
+        close(str);
+        goto fault;
+    }
+    close(str);
+
     puts("Success!");
-    //delete [] buf;
+    delete [] buf;
     return 0;
 fault:
     puts("Failure!");
-    //delete [] buf;
+    delete [] buf;
     return 1;
 }
