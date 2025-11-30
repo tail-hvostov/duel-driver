7ebab1b6c5d79c5d480a01f052ed9ed23ee17774
Tail <vitaliycof@outlook.com>
2025-12-01 00:54:09 +0300

    -Fixed tests.

--- tests/str.c
+++ tests/str.c
@@ -112,14 +112,14 @@ int main() {
     fill_buf_with_numbers(buf, buf_size);
     if (buf_size != write(str, buf, buf_size)) {
         printf("Couldn't write %jd bytes.\n", (intmax_t)buf_size);
-        close(str;
+        close(str);
         goto fault;
     }
     close(str);
     str = open(STR_FILE, O_RDONLY);
     memset(buf, 0, buf_size);
-    if (((buf_size / 2) != read(fast, buf, buf_size / 2)) ||
-        ((buf_size - buf_size / 2) != read(fast, buf + buf_size / 2, buf_size - buf_size / 2))) {
+    if (((buf_size / 2) != read(str, buf, buf_size / 2)) ||
+        ((buf_size - buf_size / 2) != read(str, buf + buf_size / 2, buf_size - buf_size / 2))) {
         printf("Couldn't read %jd bytes.\n", (intmax_t)buf_size);
         close(str);
         goto fault;
