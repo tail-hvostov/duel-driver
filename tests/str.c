848acb48dd634d4019fc3380aa0d23b5332637f2
Tail <vitaliycof@outlook.com>
2025-12-01 00:18:08 +0300

    -Added buffer size test for the string device.

---
+++ tests/str.c
@@ -0,0 +1,36 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <inttypes.h>
+
+#define STR_FILE "/dev/duel0"
+
+int main() {
+    int str;
+    off_t buf_size;
+
+    puts("1. Getting buffer size through llseek.");
+    str = open(STR_FILE, O_RDONLY);
+    if (str < 0) {
+        puts("The file did not open.");
+        goto fault;
+    }
+    if ((off_t)-1 == lseek(str, 0, SEEK_END)) {
+        puts("Unsuccessful lseek call.");
+        printf("Errno=%d.\n", errno);
+        close(str);
+        goto fault;
+    }
+    printf("Buffer size is %jd.\n", (intmax_t)buf_size);
+    close(str);
+
+    puts("Success!");
+    //delete [] buf;
+    return 0;
+fault:
+    puts("Failure!");
+    //delete [] buf;
+    return 1;
+}
