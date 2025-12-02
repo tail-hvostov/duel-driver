cfe75136ad8799505e52620dff14b0981e793063
Tail <vitaliycof@outlook.com>
2025-12-02 04:15:49 +0300

    -ops_access change.

--- duel_ops_access.c
+++ duel_ops_access.c
@@ -5,42 +5,60 @@
 static DEFINE_SPINLOCK(ops_spinlock);
 static unsigned long ops_access = 0;
 static unsigned long ops_stats = 0;
+static unsigned int str_readers = 0;
 
 int duel_request_ops(unsigned long ops, unsigned long* stats) {
     int result = 0;
+    unsigned long new_ops_access;
     spin_lock(&ops_spinlock);
 
-    if (ops_access & ops) {
+    new_ops_access = ops_access;
+    if ((ops & DUEL_OP_WRITING) & ops_access) {
         result = -EPERM;
+        goto ending;
     }
-    else {
-        ops_access = ops_access ^ ops;
-        if (NULL != stats) {
-            *stats = ops_stats;
+    new_ops_access |= (ops & DUEL_OP_WRITING);
+
+    if ((ops & DUEL_OP_RAW_WRITING) && (str_readers > 0)) {
+        result = -EPERM;
+        goto ending;
+    }
+    new_ops_access |= (ops & DUEL_OP_RAW_WRITING);
+
+    if ((ops & DUEL_OP_STR_READING) && (ops_access & DUEL_OP_RAW_WRITING)) {
+        result = -EPERM;
+        goto ending;
+    }
+
+    str_readers += (0 != (ops & DUEL_OP_STR_READING));
+    ops_access = new_ops_access;
+
+    if (NULL != stats) {
+        *stats = ops_stats;
+    }
+    //Очень тонкая работа с битами!
+    if (ops & DUEL_OP_WRITING) {
+        //Сырая запись отменяет консистентность
+        if (ops & DUEL_OP_RAW_WRITING) {
+            ops_stats &= ~DUEL_STAT_STR_CONSISTENCY;
         }
-        //Очень тонкая работа с битами!
-        if (ops & DUEL_OP_WRITING) {
-            //Сырая запись отменяет консистентность
-            if (ops & DUEL_OP_RAW_WRITING) {
-                ops_stats &= ~DUEL_STAT_STR_CONSISTENCY;
-            }
-            //Строковая запись гарантирует консистентность.
-            else {
-                ops_stats |= DUEL_STAT_STR_CONSISTENCY;
-            }
-        }
-        //Строковое чтение гарантирует консистентность
-        else if (ops & DUEL_OP_STR_READING) {
+        //Строковая запись гарантирует консистентность.
+        else {
             ops_stats |= DUEL_STAT_STR_CONSISTENCY;
         }
     }
-
+    //Строковое чтение гарантирует консистентность
+    else if (ops & DUEL_OP_STR_READING) {
+        ops_stats |= DUEL_STAT_STR_CONSISTENCY;
+    }
+ending:
     spin_unlock(&ops_spinlock);
     return result;
 }
 
 void duel_restore_ops(unsigned long ops) {
     spin_lock(&ops_spinlock);
-    ops_access = ops_access ^ ops;
+    str_readers -= (0 != (ops & DUEL_OP_STR_READING));
+    ops_access = ops_access ^ (ops & (DUEL_OP_RAW_WRITING | DUEL_OP_WRITING));
     spin_unlock(&ops_spinlock);
 }
--- tests/access.c
+++ tests/access.c
@@ -4,7 +4,7 @@
 #include <errno.h>
 
 int main() {
-    int str_file, simple_file, fast_file;
+    int str_file, simple_file, fast_file, str_file2;
 
     puts("1. Str device opening.");
     str_file = open("/dev/duel0", O_RDWR);
@@ -67,6 +67,24 @@ int main() {
     }
     close(fast_file);
     close(str_file);
+
+    puts("7. 2x str opening for reading.");
+    str_file = open("/dev/duel0", O_RDONLY);
+    str_file2 = open("/dev/duel0", O_RDONLY);
+    if (str_file2 < 0) {
+        close(str_file);
+        goto fault;
+    }
+    close(str_file2);
+    close(str_file);
+
+    puts("8. Simple device opening after str reading.");
+    simple_file = open("/dev/duel2", O_WRONLY);
+    if (simple_file < 0) {
+        printf("errno is %d.\n", errno);
+        goto fault;
+    }
+    close(simple_file);
 
     puts("Success!");
     return 0;
